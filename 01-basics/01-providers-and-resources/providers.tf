ef2f8e16707cc721368537933e9e482b1b91f46a
Patrick Mölk <pmlk.dev@gmail.com>
2025-11-28 16:25:07 +0100

    chore: pin aws provider version
    new 6.23.x causes an error with localstack

--- 01-basics/01-providers-and-resources/providers.tf
+++ 01-basics/01-providers-and-resources/providers.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 6.22.0" # 6.23.x causes errors with localstack: `operation error S3 Control: ListTagsForResource, failed to resolve service endpoint, endpoint rule error`
+    }
+  }
+}
+
 provider "aws" {
   access_key                  = "foo"
   secret_key                  = "bar"
