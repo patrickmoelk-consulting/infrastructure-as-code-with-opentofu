276ade5d78d62430cd967e12c5ec154b73004c3e
Patrick Mölk <pmlk.dev@gmail.com>
2025-12-18 00:23:20 +0100

    03-01: refactor ec2 paths with locals

--- 03-deploy-web-app/01-ec2/ec2.tf
+++ 03-deploy-web-app/01-ec2/ec2.tf
@@ -1,3 +1,16 @@
+locals {
+  remote_user          = "ubuntu"
+  remote_home_dir      = "/home/${local.remote_user}"
+  remote_app_path      = "${local.remote_home_dir}/apps/todos"
+  remote_backend_path  = "${local.remote_home_dir}/apps/todos/backend"
+  remote_frontend_path = "${local.remote_home_dir}/apps/todos/frontend"
+
+  local_app_path      = "${path.module}/../../apps"
+  local_backend_path  = "${path.module}/../../apps/backend/todos"
+  local_frontend_path = "${path.module}/../../apps/frontend/todos"
+}
+
+
 resource "aws_spot_instance_request" "todo-list-app" {
   ami                  = var.aws_ec2_ami
   instance_type        = var.aws_ec2_instance_type
@@ -7,49 +20,49 @@ resource "aws_spot_instance_request" "todo-list-app" {
 
   connection {
     type  = "ssh"
-    user  = "ubuntu"
+    user  = local.remote_user
     host  = self.public_dns
     agent = true
   }
 
   provisioner "remote-exec" {
-    inline = ["mkdir -p /home/ubuntu/apps/todos"]
+    inline = ["mkdir -p ${local.remote_app_path}"]
   }
 
   provisioner "file" {
-    source      = "${path.module}/../../apps/backend/todos"
-    destination = "/home/ubuntu/apps/todos/backend"
+    source      = local.local_backend_path
+    destination = local.remote_backend_path
   }
 
   provisioner "file" {
-    content = templatefile("${path.module}/../../apps/backend/todos/.env.prod.tftpl", {
+    content = templatefile("${local.local_backend_path}/.env.prod.tftpl", {
       db_host     = aws_db_instance.todos.address
       db_port     = aws_db_instance.todos.port
       db_name     = aws_db_instance.todos.db_name
       db_user     = aws_db_instance.todos.username
       db_password = aws_db_instance.todos.password
     })
-    destination = "/home/ubuntu/apps/todos/backend/.env.prod"
+    destination = "${local.remote_backend_path}/.env.prod"
   }
 
   provisioner "local-exec" {
-    working_dir = "${path.module}/../../apps/frontend/todos"
+    working_dir = local.local_frontend_path
     command     = "npm run build"
   }
 
   provisioner "file" {
-    source      = "${path.module}/../../apps/frontend/todos/dist"
-    destination = "/home/ubuntu/apps/todos/frontend"
+    source      = "${local.local_frontend_path}/dist"
+    destination = local.remote_frontend_path
   }
 
   provisioner "file" {
     source      = "${path.module}/todo-list-app-backend.service"
-    destination = "/home/ubuntu/apps/todos/todo-list-app-backend.service"
+    destination = "${local.remote_app_path}/todo-list-app-backend.service"
   }
 
   provisioner "file" {
-    source      = "${path.module}/../../apps/Caddyfile.prod"
-    destination = "/home/ubuntu/apps/todos/Caddyfile"
+    source      = "${local.local_app_path}/Caddyfile.prod"
+    destination = "${local.remote_app_path}/Caddyfile"
   }
 
   provisioner "remote-exec" {
@@ -65,4 +78,3 @@ resource "aws_key_pair" "todo-list" {
 data "local_file" "todo-list-public-key" {
   filename = "/Users/patrick/.ssh/aws-iac-workshop.ed25519.pub"
 }
-
