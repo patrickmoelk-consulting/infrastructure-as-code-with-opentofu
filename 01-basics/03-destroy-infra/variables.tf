4fb95cdac31f588dd2d166e6a00c39d8834f5e1a
Patrick Mölk <pmlk.dev@gmail.com>
2026-01-06 22:47:04 +0100

    01: refine default bucket name prefixes with missing chapter

--- 01-basics/03-destroy-infra/variables.tf
+++ 01-basics/03-destroy-infra/variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "s3_bucket_name" {
   type        = string
   description = "Name of the S3 bucket"
-  default     = "iac-workshop-bucket-03-YOUR-NAME"
+  default     = "iac-workshop-bucket-01-03-YOUR-NAME"
 }
 
 variable "environment" {
