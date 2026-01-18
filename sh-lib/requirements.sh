1e4362079e45f286458112a1f1871c976cc9be42
Patrick Mölk <pmlk.dev@gmail.com>
2026-01-18 17:51:53 +0100

    fix requirements.sh to respect AWS_PROFILE env var

--- sh-lib/requirements.sh
+++ sh-lib/requirements.sh
@@ -9,8 +9,9 @@ function check_aws_cli() {
 	if [[ -z "$AWS_PROFILE" ]]; then
 		echo -e "${YELLOW_COLOR}WARNING: environment variable AWS_PROFILE is NOT set, falling back to the \"default\" profile.${CLEAR_COLOR}";
 	fi
-
-	if [[ ! $(aws sts get-caller-identity > /dev/null) ]]; then
+	
+	aws sts get-caller-identity > /dev/null
+	if [[ $? -ne 0 ]]; then
 		echo -e "${RED_COLOR}ERROR: you must have an AWS CLI profile named \"${AWS_PROFILE-default}\" in your ~/.aws/config file. In case you're using SSO, you also need to be signed in, e.g. using aws-sso-util login --profile ${AWS_PROFILE-default}.${CLEAR_COLOR}" >&2;
 		exit 1;
 	fi
